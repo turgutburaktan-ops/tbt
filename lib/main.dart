f318cd6841d2de3b522f61cff0e7f5ae83c1b65d
turgutburaktan-ops <turgutburaktan@gmail.com>
2026-08-09 15:08:57 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'services/favorites_service.dart';
 import 'screens/home_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp();
-
-  import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-
-import 'screens/home_screen.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp();
-
-  runApp(const BestPhotoSpotApp());
-}
-
-class BestPhotoSpotApp extends StatelessWidget {
-  const BestPhotoSpotApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'En İyi Çekim Noktası',
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        scaffoldBackgroundColor: const Color(0xFF080B10),
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFFFFC107),
-          brightness: Brightness.dark,
-        ),
-        useMaterial3: true,
-      ),
-      home: const HomeScreen(),
-    );
-  }
-}
 
   runApp(const BestPhotoSpotApp());
 }
