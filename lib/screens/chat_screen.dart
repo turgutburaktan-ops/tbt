865cbc7a145051039b29b3da66988a45ce1e6a7e
turgutburaktan-ops <turgutburaktan@gmail.com>
2026-08-13 18:05:28 +0300

    Keep chat screen backward compatible

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -13,7 +13,7 @@ class ChatScreen extends StatefulWidget {
   const ChatScreen({
     super.key,
     required this.otherUserId,
-    required this.otherDisplayName,
+    this.otherDisplayName = 'Kullanıcı',
     this.sourceType,
     this.sourceId,
   });
