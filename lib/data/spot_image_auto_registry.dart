b1c0c86f2c689e70ae9d6a380d94202509f8aa88
turgutburaktan-ops <turgutburaktan@gmail.com>
2026-08-14 20:00:47 +0300

    Add generated nationwide spot image registry

---
+++ lib/data/spot_image_auto_registry.dart
@@ -0,0 +1,6 @@
+import 'spot_image_registry.dart';
+
+/// Otomatik olarak lisanslı kaynaklardan çözümlenen Türkiye geneli çekim noktası
+/// görselleri. Bu dosya `tools/build_spot_image_catalog.py` tarafından yeniden
+/// oluşturulur. Elle düzenlenmemelidir.
+const spotImageAutoRegistry = <String, SpotImageInfo>{};
