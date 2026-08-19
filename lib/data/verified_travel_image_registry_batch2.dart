c6d4aace83271373065cd40db3f5f376e054a6df
turgutburaktan-ops <turgutburaktan@gmail.com>
2026-08-19 20:25:41 +0300

    Add verified images for second travel batch

---
+++ lib/data/verified_travel_image_registry_batch2.dart
@@ -0,0 +1,90 @@
+import 'spot_image_registry.dart';
+
+/// İkinci doğrulanmış gezilecek yer grubu için elle kontrol edilmiş gerçek
+/// fotoğraflar. Her kayıt kaynak sayfası, yazar ve açık lisans bilgisi taşır.
+const verifiedTravelImageRegistryBatch2 = <String, SpotImageInfo>{
+  'sultanahmet-camii': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Exterior_of_Sultan_Ahmed_I_Mosque_in_Istanbul%2C_Turkey_002.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Moonik',
+    license: 'CC BY-SA 3.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Exterior_of_Sultan_Ahmed_I_Mosque_in_Istanbul%2C_Turkey_002.jpg',
+  ),
+  'yerebatan-sarnici': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Cisterna_Bas%C3%ADlica%2C_Estambul%2C_Turqu%C3%ADa%2C_2024-09-28%2C_DD_58-60_HDR.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Diego Delso',
+    license: 'CC BY-SA 4.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Cisterna_Bas%C3%ADlica%2C_Estambul%2C_Turqu%C3%ADa%2C_2024-09-28%2C_DD_58-60_HDR.jpg',
+  ),
+  'topkapi-sarayi': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Topkapi_Palace_Seen_From_Harem.JPG?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Bjørn Christian Tørrissen',
+    license: 'CC BY-SA 3.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Topkapi_Palace_Seen_From_Harem.JPG',
+  ),
+  'troya': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/A_view_from_Troy_Ancient_City%2C_%C3%87anakkale%2C_Turkey.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Kadı',
+    license: 'CC BY-SA 4.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:A_view_from_Troy_Ancient_City%2C_%C3%87anakkale%2C_Turkey.jpg',
+  ),
+  'bergama-akropol': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Pergamon_Acropolis.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Ingo Mehling',
+    license: 'CC BY-SA 3.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Pergamon_Acropolis.jpg',
+  ),
+  'aphrodisias': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Aphrodisias_turkey.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Brocken Inaglory',
+    license: 'CC BY-SA 4.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Aphrodisias_turkey.jpg',
+  ),
+  'gobeklitepe': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/G%C3%B6bekli_Tepe%2C_Urfa.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Teomancimit',
+    license: 'CC BY-SA 3.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:G%C3%B6bekli_Tepe%2C_Urfa.jpg',
+  ),
+  'ani-oren-yeri': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Ani_Archeological_Site%2C_March_2020_1.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: "Héctor Ochoa 'Robot8A'",
+    license: 'CC BY-SA 4.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Ani_Archeological_Site%2C_March_2020_1.jpg',
+  ),
+  'ishak-pasa-sarayi': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/00_3411_%C4%B0shak_Pa%C5%9Fa_Saray%C4%B1_-_Turkey.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'W. Bulach',
+    license: 'CC BY-SA 4.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:00_3411_%C4%B0shak_Pa%C5%9Fa_Saray%C4%B1_-_Turkey.jpg',
+  ),
+  'akdamar-kilisesi': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/00_0390_Lake_Van%2C_church_on_Akdamar_Island.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'W. Bulach',
+    license: 'CC BY-SA 4.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:00_0390_Lake_Van%2C_church_on_Akdamar_Island.jpg',
+  ),
+  'mardin-tarihi-merkez': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Mardin_old_town.JPG?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Florian Koch',
+    license: 'CC BY-SA 2.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Mardin_old_town.JPG',
+  ),
+  'hattusa': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Lion_Gate%2C_Hattusa_01.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Bernard Gagnon',
+    license: 'CC BY-SA 3.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Lion_Gate%2C_Hattusa_01.jpg',
+  ),
+};
