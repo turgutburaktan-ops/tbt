c8e5f439cecd09da421fe6d03a21d2f69582905a
turgutburaktan-ops <turgutburaktan@gmail.com>
2026-08-19 11:01:00 +0300

    feat: gate new accounts through onboarding

---
+++ lib/screens/app_entry_gate.dart
@@ -0,0 +1,47 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'home_shell_screen.dart';
+import 'student_onboarding_screen.dart';
+
+class AppEntryGate extends StatelessWidget {
+  const AppEntryGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      initialData: FirebaseAuth.instance.currentUser,
+      builder: (context, authSnapshot) {
+        final user = authSnapshot.data;
+        if (user == null) return const HomeScreen();
+
+        return StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+          stream: FirebaseFirestore.instance
+              .collection('users')
+              .doc(user.uid)
+              .snapshots(),
+          builder: (context, profileSnapshot) {
+            if (profileSnapshot.connectionState == ConnectionState.waiting &&
+                !profileSnapshot.hasData) {
+              return const Scaffold(
+                backgroundColor: Color(0xFF090A0C),
+                body: Center(child: CircularProgressIndicator()),
+              );
+            }
+
+            final data = profileSnapshot.data?.data();
+            final onboardingRequired = data?['onboardingRequired'] == true;
+            final onboardingCompleted = data?['onboardingCompleted'] == true;
+
+            if (onboardingRequired && !onboardingCompleted) {
+              return const StudentOnboardingScreen();
+            }
+            return const HomeScreen();
+          },
+        );
+      },
+    );
+  }
+}
