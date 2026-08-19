0738482dcf4d2b0967bef4b4126d5006c35e1b9a
turgutburaktan-ops <turgutburaktan@gmail.com>
2026-08-19 11:05:36 +0300

    feat: handle incoming app deep links

---
+++ lib/services/deep_link_service.dart
@@ -0,0 +1,61 @@
+import 'dart:async';
+
+import 'package:app_links/app_links.dart';
+import 'package:flutter/material.dart';
+
+import '../screens/community_profile_screen.dart';
+import '../screens/event_deep_link_screen.dart';
+
+class DeepLinkService {
+  DeepLinkService._();
+  static final DeepLinkService instance = DeepLinkService._();
+
+  final AppLinks _appLinks = AppLinks();
+  StreamSubscription<Uri>? _subscription;
+  final Set<String> _recent = <String>{};
+
+  void start(GlobalKey<NavigatorState> navigatorKey) {
+    _subscription?.cancel();
+    _subscription = _appLinks.uriLinkStream.listen((uri) {
+      _open(uri, navigatorKey);
+    });
+  }
+
+  void dispose() {
+    _subscription?.cancel();
+    _subscription = null;
+  }
+
+  void _open(Uri uri, GlobalKey<NavigatorState> navigatorKey) {
+    if (uri.scheme != 'tbt') return;
+    final key = uri.toString();
+    if (_recent.contains(key)) return;
+    _recent.add(key);
+    Future<void>.delayed(const Duration(seconds: 2), () => _recent.remove(key));
+
+    final id = uri.pathSegments.isEmpty ? '' : uri.pathSegments.first.trim();
+    if (id.isEmpty) return;
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final navigator = navigatorKey.currentState;
+      if (navigator == null) return;
+
+      switch (uri.host) {
+        case 'community':
+          navigator.push(
+            MaterialPageRoute(
+              builder: (_) => CommunityProfileScreen(communityId: id),
+            ),
+          );
+          break;
+        case 'event':
+          navigator.push(
+            MaterialPageRoute(
+              builder: (_) => EventDeepLinkScreen(eventId: id),
+            ),
+          );
+          break;
+      }
+    });
+  }
+}
