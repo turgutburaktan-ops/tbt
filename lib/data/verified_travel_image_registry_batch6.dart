eb234654df808b4ac1c7687a25de2d966053e4bb
turgutburaktan-ops <turgutburaktan@gmail.com>
2026-08-20 05:10:52 +0300

    Add verified travel images for batch six

---
+++ lib/data/verified_travel_image_registry_batch6.dart
@@ -0,0 +1,89 @@
+import 'spot_image_registry.dart';
+
+/// Altıncı doğrulanmış gezi grubu için elle kontrol edilmiş gerçek görseller.
+const verifiedTravelImageRegistryBatch6 = <String, SpotImageInfo>{
+  'zilkale': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Zilkale%2C_%C3%87aml%C4%B1hem%C5%9Fin_%28cropped%29.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Hamdigumus',
+    license: 'CC0 1.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Zilkale%2C_%C3%87aml%C4%B1hem%C5%9Fin_%28cropped%29.jpg',
+  ),
+  'giresun-kalesi': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/G%C4%B0RESUN_KALES%C4%B0.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Girtab2828',
+    license: 'CC BY-SA 4.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:G%C4%B0RESUN_KALES%C4%B0.jpg',
+  ),
+  'trabzon-ataturk-kosku': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Atatiurko_namas.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Aleksasfi',
+    license: 'Public domain',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Atatiurko_namas.jpg',
+  ),
+  'erzurum-cifte-minareli-medrese': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/%C3%87ifte_Minareli_Medrese_%28Erzurum%29_Exterior_front_side_in_2006_3278_straightened.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Dosseman',
+    license: 'CC BY-SA 4.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:%C3%87ifte_Minareli_Medrese_%28Erzurum%29_Exterior_front_side_in_2006_3278_straightened.jpg',
+  ),
+  'van-kalesi': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Van_fortress_eastern_walls_014.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Dosseman',
+    license: 'CC BY-SA 4.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Van_fortress_eastern_walls_014.jpg',
+  ),
+  'arslantepe': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Arslantepe1.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Klaus-Peter Simon',
+    license: 'CC BY-SA 3.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Arslantepe1.jpg',
+  ),
+  'mor-gabriel-manastiri': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Monastery_Mor_Gabriel_Turkey.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Christian Koehn',
+    license: 'CC BY-SA 2.0 DE',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Monastery_Mor_Gabriel_Turkey.jpg',
+  ),
+  'girlevik-selalesi': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Girlevik_Waterfall%2C_Erzincan%2C_Turkey.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Medgeorgia',
+    license: 'CC BY-SA 4.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Girlevik_Waterfall%2C_Erzincan%2C_Turkey.jpg',
+  ),
+  'rumkale': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Hromkla_Halfeti.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Ferrus',
+    license: 'Public domain',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Hromkla_Halfeti.jpg',
+  ),
+  'cavustepe-kalesi': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Urartian_fort_in_%C3%87avu%C5%9Ftepe.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Martijn Munneke',
+    license: 'CC BY 2.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Urartian_fort_in_%C3%87avu%C5%9Ftepe.jpg',
+  ),
+  'tortum-selalesi': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Tortum_Waterfall_%282%29.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Jaba1977',
+    license: 'CC BY-SA 4.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Tortum_Waterfall_%282%29.jpg',
+  ),
+  'karanlik-kanyon': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Canyone_2_%2832828345%29.jpeg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Engin Asil',
+    license: 'CC BY-SA 3.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Canyone_2_%2832828345%29.jpeg',
+  ),
+};
