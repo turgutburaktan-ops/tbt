fdea5523610f67c6d186c492188a73cad1520162
turgutburaktan-ops <turgutburaktan@gmail.com>
2026-08-20 07:29:03 +0300

    Add verified travel images for batch eight

---
+++ lib/data/verified_travel_image_registry_batch8.dart
@@ -0,0 +1,90 @@
+import 'spot_image_registry.dart';
+
+/// Sekizinci doğrulanmış gezi grubunun elle seçilmiş gerçek fotoğrafları.
+/// Kaynak, fotoğrafçı ve lisans bilgileri dosyayla birlikte tutulur.
+const verifiedTravelImageRegistryBatch8 = <String, SpotImageInfo>{
+  'sardes-antik-kenti': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/The_Bath-Gymnasium_complex_at_Sardis%2C_late_2nd_-_early_3rd_century_AD%2C_Sardis%2C_Turkey_%2817098680002%29.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Carole Raddato',
+    license: 'CC BY-SA 2.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:The_Bath-Gymnasium_complex_at_Sardis%2C_late_2nd_-_early_3rd_century_AD%2C_Sardis%2C_Turkey_%2817098680002%29.jpg',
+  ),
+  'laodikeia': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/TR_Pamukkale_Laodicea_asv2020-02_img11.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'A.Savin',
+    license: 'Free Art License',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:TR_Pamukkale_Laodicea_asv2020-02_img11.jpg',
+  ),
+  'sagalassos': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Sagalassos_Ancient_City.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'LeoniKalkis',
+    license: 'CC BY-SA 4.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Sagalassos_Ancient_City.jpg',
+  ),
+  'kibyra': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Kibyra_Theatre_9919.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Dosseman',
+    license: 'CC BY-SA 4.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Kibyra_Theatre_9919.jpg',
+  ),
+  'phaselis': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Phaselis_axb01.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Alexander Buschorn',
+    license: 'CC BY-SA 3.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Phaselis_axb01.jpg',
+  ),
+  'olympos-antik-kenti': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Olympos_Ancient_City.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Capyusuf',
+    license: 'CC BY-SA 4.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Olympos_Ancient_City.jpg',
+  ),
+  'termessos': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Termessos_-_Theater.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Ingo Mehling',
+    license: 'CC BY-SA 3.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Termessos_-_Theater.jpg',
+  ),
+  'side-antik-kenti': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Roman_theatre_Side_Turkey.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Helen Simonsson',
+    license: 'CC BY-SA 4.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Roman_theatre_Side_Turkey.jpg',
+  ),
+  'aizanoi': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Aizanoi.ZeusTemple.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'China_Crisis',
+    license: 'CC BY-SA 2.5',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Aizanoi.ZeusTemple.jpg',
+  ),
+  'priene': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Temple_of_Athena_at_Priene.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Ken and Nyetta',
+    license: 'CC BY 2.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Temple_of_Athena_at_Priene.jpg',
+  ),
+  'miletos': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/The_Theater_of_Miletus.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Jiuguang Wang',
+    license: 'CC BY-SA 2.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:The_Theater_of_Miletus.jpg',
+  ),
+  'didyma-apollon': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Didyma_Temple_of_Apollo_pronaos_01.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Mark Landon',
+    license: 'CC BY 4.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Didyma_Temple_of_Apollo_pronaos_01.jpg',
+  ),
+};
