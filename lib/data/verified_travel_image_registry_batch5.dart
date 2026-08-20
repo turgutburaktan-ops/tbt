af3f9e34f54bb97e3a82afb08d1e388d184dfb2c
turgutburaktan-ops <turgutburaktan@gmail.com>
2026-08-20 05:03:42 +0300

    Add verified travel images for batch five

---
+++ lib/data/verified_travel_image_registry_batch5.dart
@@ -0,0 +1,89 @@
+import 'spot_image_registry.dart';
+
+/// Beşinci doğrulanmış gezi grubu için elle kontrol edilmiş gerçek görseller.
+const verifiedTravelImageRegistryBatch5 = <String, SpotImageInfo>{
+  'edirne-selimiye': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Edirne_7333_Nevit.JPG?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Nevit Dilmen',
+    license: 'CC BY-SA 3.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Edirne_7333_Nevit.JPG',
+  ),
+  'bursa-ulu-camii': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/71_Bursa_la_Grande_Moschea_%28Edited%29.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Beñat Irasuegi',
+    license: 'CC BY-SA 4.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:71_Bursa_la_Grande_Moschea_%28Edited%29.jpg',
+  ),
+  'cumalikizik': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Cumal%C4%B1k%C4%B1z%C4%B1k_7121.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Darwinek',
+    license: 'CC BY-SA 3.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Cumal%C4%B1k%C4%B1z%C4%B1k_7121.jpg',
+  ),
+  'divrigi-ulu-camii': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Divrigi_complex_DSCF3028.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'R Prazeres',
+    license: 'CC BY-SA 4.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Divrigi_complex_DSCF3028.jpg',
+  ),
+  'varda-koprusu': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Varda_Demiryolu_Koprusu.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'MUSTAFA TOR',
+    license: 'CC BY-SA 2.5',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Varda_Demiryolu_Koprusu.jpg',
+  ),
+  'malabadi-koprusu': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Malabadibridge.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Dyrt',
+    license: 'CC BY-SA 4.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Malabadibridge.jpg',
+  ),
+  'amasya-kral-kaya-mezarlari': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Kral_Kaya_Mezarlar%C4%B1_%28Amasya%29.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Cobija',
+    license: 'CC BY-SA 4.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Kral_Kaya_Mezarlar%C4%B1_%28Amasya%29.jpg',
+  ),
+  'sinop-tarihi-cezaevi': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Sinop_Tarihi_Cezaevi_1.JPG?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Basak',
+    license: 'CC BY-SA 4.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Sinop_Tarihi_Cezaevi_1.JPG',
+  ),
+  'izmir-saat': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Izmir_square_clock_tower.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Sailko',
+    license: 'CC BY-SA 3.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Izmir_square_clock_tower.jpg',
+  ),
+  'canakkale-sehitler-abidesi': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Onderkokturk-canakkale01.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Onder Kokturk',
+    license: 'CC BY-SA 4.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Onderkokturk-canakkale01.jpg',
+  ),
+  'bozcaada-kalesi': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Bozcaada.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Filanca',
+    license: 'CC BY-SA 3.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Bozcaada.jpg',
+  ),
+  'diyarbakir-ulu-camii': SpotImageInfo(
+    networkUrl: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Diyarbakir_Ulu_Cami_2022.jpg?width=1280',
+    sourceName: 'Wikimedia Commons',
+    author: 'Kaya Elifnur 72',
+    license: 'CC BY-SA 4.0',
+    sourcePage: 'https://commons.wikimedia.org/wiki/File:Diyarbakir_Ulu_Cami_2022.jpg',
+  ),
+};
