680a0c1610d39499c225503312450d3e6c456827
turgutburaktan-ops <turgutburaktan@gmail.com>
2026-08-20 05:13:15 +0300

    Localize sixth verified travel image batch

--- tool/localize_spot_images.py
+++ tool/localize_spot_images.py
@@ -24,6 +24,7 @@ REGISTRIES = [
     ROOT / "lib/data/verified_travel_image_registry_batch3.dart",
     ROOT / "lib/data/verified_travel_image_registry_batch4.dart",
     ROOT / "lib/data/verified_travel_image_registry_batch5.dart",
+    ROOT / "lib/data/verified_travel_image_registry_batch6.dart",
 ]
 ASSET_DIR = ROOT / "assets/spots"
 ATTRIBUTION = ASSET_DIR / "ATTRIBUTION.md"
