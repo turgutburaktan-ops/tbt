dff2702520ba884ed25dafcc631f9cc4ce531f72
turgutburaktan-ops <turgutburaktan@gmail.com>
2026-08-21 09:52:37 +0300

    Add unified route place model

---
+++ lib/models/route_place.dart
@@ -0,0 +1,15 @@
+class RoutePlace {
+  final String id;
+  final String name;
+  final String category;
+  final double latitude;
+  final double longitude;
+
+  const RoutePlace({
+    required this.id,
+    required this.name,
+    required this.category,
+    required this.latitude,
+    required this.longitude,
+  });
+}
