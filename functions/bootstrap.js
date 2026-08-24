bb932a271c664433514dd8bc4a01794f46baed60
turgutburaktan-ops <turgutburaktan@gmail.com>
2026-08-24 12:07:58 +0300

    feat: register event cover function

--- functions/bootstrap.js
+++ functions/bootstrap.js
@@ -1,3 +1,4 @@
 Object.assign(exports, require('./index'));
 Object.assign(exports, require('./business'));
 Object.assign(exports, require('./early_business_access'));
+Object.assign(exports, require('./event_cover'));
