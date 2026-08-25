3b0cd8bc6c7ffd1a213b64ae302fdd3332a86a2f
turgutburaktan-ops <turgutburaktan@gmail.com>
2026-08-25 13:11:54 +0300

    feat: export missing business submission flow

--- functions/bootstrap.js
+++ functions/bootstrap.js
@@ -4,6 +4,7 @@ Object.assign(exports, require('./business_content'));
 Object.assign(exports, require('./business_enhancements'));
 Object.assign(exports, require('./business_growth'));
 Object.assign(exports, require('./business_maintenance'));
+Object.assign(exports, require('./business_candidate'));
 Object.assign(exports, require('./admin_business_premium'));
 Object.assign(exports, require('./early_business_access'));
 Object.assign(exports, require('./event_cover'));
