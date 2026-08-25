a16536b73cdc6cdd82ece7212d7434cdb33bbe14
turgutburaktan-ops <turgutburaktan@gmail.com>
2026-08-26 02:26:50 +0300

    Rename dining category to Lezzet

--- lib/models/nearby_venue.dart
+++ lib/models/nearby_venue.dart
@@ -2,7 +2,7 @@ enum NearbyVenueCategory { dining, cafe, hotel }
 
 extension NearbyVenueCategoryX on NearbyVenueCategory {
   String get label => switch (this) {
-        NearbyVenueCategory.dining => 'Yeme-İçme',
+        NearbyVenueCategory.dining => 'Lezzet',
         NearbyVenueCategory.cafe => 'Kafeler',
         NearbyVenueCategory.hotel => 'Oteller',
       };
@@ -34,46 +34,13 @@ class NearbyVenue {
   final String phone;
   final String website;
 
-  const NearbyVenue({
-    required this.id,
-    required this.category,
-    required this.name,
-    required this.latitude,
-    required this.longitude,
-    this.address = '',
-    this.openingHours = '',
-    this.phone = '',
-    this.website = '',
-  });
+  const NearbyVenue({required this.id,required this.category,required this.name,required this.latitude,required this.longitude,this.address='',this.openingHours='',this.phone='',this.website=''});
 
-  Map<String, Object?> toJson() => {
-        'id': id,
-        'category': category.name,
-        'name': name,
-        'latitude': latitude,
-        'longitude': longitude,
-        'address': address,
-        'openingHours': openingHours,
-        'phone': phone,
-        'website': website,
-      };
+  Map<String,Object?> toJson()=>{'id':id,'category':category.name,'name':name,'latitude':latitude,'longitude':longitude,'address':address,'openingHours':openingHours,'phone':phone,'website':website};
 
-  factory NearbyVenue.fromJson(Map<String, dynamic> json) {
-    final categoryName = (json['category'] ?? '').toString();
-    final category = NearbyVenueCategory.values.firstWhere(
-      (value) => value.name == categoryName,
-      orElse: () => NearbyVenueCategory.dining,
-    );
-    return NearbyVenue(
-      id: (json['id'] ?? '').toString(),
-      category: category,
-      name: (json['name'] ?? '').toString(),
-      latitude: (json['latitude'] as num?)?.toDouble() ?? 0,
-      longitude: (json['longitude'] as num?)?.toDouble() ?? 0,
-      address: (json['address'] ?? '').toString(),
-      openingHours: (json['openingHours'] ?? '').toString(),
-      phone: (json['phone'] ?? '').toString(),
-      website: (json['website'] ?? '').toString(),
-    );
+  factory NearbyVenue.fromJson(Map<String,dynamic> json){
+    final categoryName=(json['category']??'').toString();
+    final category=NearbyVenueCategory.values.firstWhere((value)=>value.name==categoryName,orElse:()=>NearbyVenueCategory.dining);
+    return NearbyVenue(id:(json['id']??'').toString(),category:category,name:(json['name']??'').toString(),latitude:(json['latitude'] as num?)?.toDouble()??0,longitude:(json['longitude'] as num?)?.toDouble()??0,address:(json['address']??'').toString(),openingHours:(json['openingHours']??'').toString(),phone:(json['phone']??'').toString(),website:(json['website']??'').toString());
   }
 }
