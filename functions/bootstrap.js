78a12f766293ddb8b3cab0d52cb2ca6c0652b8ba
turgutburaktan-ops <turgutburaktan@gmail.com>
2026-08-25 12:52:45 +0300

    feat: export admin business premium functions

--- functions/bootstrap.js
+++ functions/bootstrap.js
@@ -2,5 +2,6 @@ Object.assign(exports, require('./index'));
 Object.assign(exports, require('./business'));
 Object.assign(exports, require('./business_content'));
 Object.assign(exports, require('./business_growth'));
+Object.assign(exports, require('./admin_business_premium'));
 Object.assign(exports, require('./early_business_access'));
 Object.assign(exports, require('./event_cover'));
