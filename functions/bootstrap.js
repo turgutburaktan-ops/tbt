2047e192201a219f9eab01c692623f4ccf536277
turgutburaktan-ops <turgutburaktan@gmail.com>
2026-08-25 13:00:14 +0300

    feat: export business enhancements

--- functions/bootstrap.js
+++ functions/bootstrap.js
@@ -1,6 +1,7 @@
 Object.assign(exports, require('./index'));
 Object.assign(exports, require('./business'));
 Object.assign(exports, require('./business_content'));
+Object.assign(exports, require('./business_enhancements'));
 Object.assign(exports, require('./business_growth'));
 Object.assign(exports, require('./admin_business_premium'));
 Object.assign(exports, require('./early_business_access'));
