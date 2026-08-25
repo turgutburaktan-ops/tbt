01b40b8842a60c7c28ee6559cfa7608413487d0f
turgutburaktan-ops <turgutburaktan@gmail.com>
2026-08-25 10:59:50 +0300

    feat: export business content management functions

--- functions/bootstrap.js
+++ functions/bootstrap.js
@@ -1,4 +1,5 @@
 Object.assign(exports, require('./index'));
 Object.assign(exports, require('./business'));
+Object.assign(exports, require('./business_content'));
 Object.assign(exports, require('./early_business_access'));
 Object.assign(exports, require('./event_cover'));
