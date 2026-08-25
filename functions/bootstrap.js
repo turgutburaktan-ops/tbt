5d019fc42af131669682f951244ba89efdcf683a
turgutburaktan-ops <turgutburaktan@gmail.com>
2026-08-25 13:03:09 +0300

    feat: export business menu media backend

--- functions/bootstrap.js
+++ functions/bootstrap.js
@@ -2,6 +2,7 @@ Object.assign(exports, require('./index'));
 Object.assign(exports, require('./business'));
 Object.assign(exports, require('./business_content'));
 Object.assign(exports, require('./business_enhancements'));
+Object.assign(exports, require('./business_profile_extra'));
 Object.assign(exports, require('./business_growth'));
 Object.assign(exports, require('./admin_business_premium'));
 Object.assign(exports, require('./early_business_access'));
