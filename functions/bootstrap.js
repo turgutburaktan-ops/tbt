97b808ff41b0d91fbadc9f0c8adf8b3b47324452
turgutburaktan-ops <turgutburaktan@gmail.com>
2026-09-03 12:46:38 -0700

    Restore public invocation for Firebase web callables

--- functions/bootstrap.js
+++ functions/bootstrap.js
@@ -1,3 +1,9 @@
+const {setGlobalOptions} = require('firebase-functions/v2');
+
+// Callable functions must be reachable from the public web; application-level
+// authentication and authorization remain enforced inside each handler.
+setGlobalOptions({invoker: 'public'});
+
 Object.assign(exports, require('./index'));
 Object.assign(exports, require('./business'));
 Object.assign(exports, require('./business_content'));
