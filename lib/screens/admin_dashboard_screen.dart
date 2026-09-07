0b99da99b4e9eceb703610509e0b12df3ccdb98c
turgutburaktan-ops <turgutburaktan@gmail.com>
2026-09-07 08:17:37 -0700

    fix(admin): retain document link launcher

--- lib/screens/admin_dashboard_screen.dart
+++ lib/screens/admin_dashboard_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../theme/app_theme.dart';
 import '../services/admin_access.dart';
